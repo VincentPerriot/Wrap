a1a1182ba6d58e511d46e2201f1aad69d5ffc0e9
Vincent Perriot <32515747+VincentPerriot@users.noreply.github.com>
2024-10-09 00:14:34 +0200

    Test Install Vulkan

--- Wrap/main.cpp
+++ Wrap/main.cpp
@@ -1,8 +1,26 @@
-#include<iostream>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
-int main()
-{
-	std::cout << "Hello World" << std::endl;
+#include <iostream>
 
-	return 0;
+int main() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
 }
